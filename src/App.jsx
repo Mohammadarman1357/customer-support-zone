b118e6c61fb95940e7ba44b0e0dbd54d71a25148
Arman <mohammadjahedulislamarman@gmail.com>
2025-09-25 00:52:07 +0600

    added json fake data and fetch the data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 
+const fetchCustomer = async () => {
+  const res = await fetch("/customer.json");
+  return res.json()
+}
+const customerPromise = fetchCustomer()
+console.log(customerPromise)
+
 function App() {
 
   return (
     <>
 
       <Navbar></Navbar>
-
-
     </>
   )
 }
