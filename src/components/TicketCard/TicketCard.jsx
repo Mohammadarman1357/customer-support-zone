ea6a0363d1ed4b21d63c1421605848373c9eb2a3
Arman <mohammadjahedulislamarman@gmail.com>
2025-09-26 12:31:05 +0600

    design priority and status button and apply ternary condition here

--- src/components/TicketCard/TicketCard.jsx
+++ src/components/TicketCard/TicketCard.jsx
@@ -12,14 +12,22 @@ const TicketCard = ({ customer }) => {
         <div onClick={() => { handleTicket(customer.title) }} className='bg-white p-4 rounded-[4px] col-span-1 shadow-md space-y-2'>
             <div className='flex items-center justify-between'>
                 <h3>{customer.title}</h3>
-                <button className='bg-[#B9F8CF] px-2 py-1 rounded-[400px] font-medium text-xs'><span className='text-[#02A53B]'><i className="fa-solid fa-circle"></i></span><span className='text-[#0B5E06] ml-1'>{customer.status}</span></button>
+                <button className={`px-2 py-1 rounded-[400px] font-medium text-xs ${customer.status === "Open" ? "bg-[#B9F8CF]" : customer.status === "In Progress" ? "bg-[#F8F3B9]" : "bg-black"}`}>
+                    <span className={`${customer.status === "Open" ? "text-[#0B5E06]" : customer.status === "In Progress" ? "text-[#9C7700]" : "text-white"}`}>
+                        <span className={`${customer.status === "Open" ? "text-[#02A53B] mr-1" : customer.status === "In Progress" ? "text-[#FEBB0C] mr-1" : "text-white mr-1"}`}><i className="fa-solid fa-circle"></i></span>
+                        {customer.status}</span>
+                </button>
             </div>
             <h1 className='text-[#627382]'>{customer.description}</h1>
 
             <div className='flex justify-between'>
                 <div className='flex'>
                     <h3 className='text-[#627382] text-[14px] font-medium'>{customer.id}</h3>
-                    <h3 className='text-[#F83044] text-[14px] font-medium ml-2'>{customer.priority}</h3>
+                    <h3 className={`text-[#F83044] text-[14px] font-medium ml-2
+                    ${customer.priority === "HIGH PRIORITY" ? "text-[#F83044]" : customer.priority === "MEDIUM PRIORITY" ? "text-[#FEBB0C]" : customer.priority === "LOW PRIORITY" ? "text-[#02A53B]" : "text-blue-800"}
+                        `}>
+                        {customer.priority}
+                    </h3>
                 </div>
                 <div className='flex'>
                     <h3 className='text-[#627382] text-[14px] mr-2'>{customer.customer}</h3>
